22d16399a1abd2cd43ddeee9be319b7d7dada34b
sathish <sathishsacha111@gmail.com>
2024-08-18 01:59:25 +0530

    Added

---
+++ learning/Array.java
@@ -0,0 +1,10 @@
+import java.util.*;
+
+public class Array {
+        public static void main(String[] args) {
+                String[] test = {"hello" , "wel" , "to"};
+                for (int i = 0 ; i < test.length ; i++) {
+                        System.out.println(test[i]);
+                }
+        }
+}
