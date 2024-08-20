6526ad0cc74f46510b2afcc65fb54289170b9624
sathish <sathishsacha111@gmail.com>
2024-08-20 21:52:22 +0530

    Added even or odd

---
+++ learning/even_odd_prob.java
@@ -0,0 +1,13 @@
+import java.util.*;
+
+public class even_odd_prob {
+	public static void main(String[] args) {
+		int time = 30;
+
+		if (time > 40 ) {
+			System.out.println("if time");
+		} else {
+			System.out.println("else time");
+		}
+	}
+}
