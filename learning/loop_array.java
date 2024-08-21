c69834cf3dbbbdf16747e7f5337800b85c339f02
sathish <sathishsacha111@gmail.com>
2024-08-21 21:42:43 +0530

    Added loop_array

---
+++ learning/loop_array.java
@@ -0,0 +1,11 @@
+import java.util.*;
+
+public class loop_array {
+       public static void main(String[] args) {
+	       String[] cars = {"bmw" , "tata", "jag"};
+
+	       for (int i=0 ; i < cars.length ; i++) {
+		       System.out.println(cars[i]);
+	       }
+       }
+}
