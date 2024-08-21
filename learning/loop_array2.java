59372054a51af22919193a52994a8e8a7f264a99
sathish <sathishsacha111@gmail.com>
2024-08-21 21:46:52 +0530

    Added another method of looping in array

---
+++ learning/loop_array2.java
@@ -0,0 +1,11 @@
+import java.util.*;
+
+public class loop_array2 {
+	public static void main(String[] args) {
+		String cars[] = {"tata","jag"};
+
+		for (String i : cars) {
+			System.out.println(i);
+		}
+	}
+}
