294b1ed20a504013aee6760d1baae49329edc589
sathish <sathishsacha111@gmail.com>
2024-08-22 22:14:57 +0530

    Added methods basic

---
+++ learning/basic_method.java
@@ -0,0 +1,11 @@
+import java.util.*;
+
+public class basic_method {
+	static void firstMethod() {
+		System.out.println("first method");
+	}
+
+	public static void main(String[] args) {
+		firstMethod();
+	}
+}
