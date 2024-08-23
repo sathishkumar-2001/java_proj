552a1b7935f1ec66a66f1c4db55ad30093527cc1
sathish <sathishsacha111@gmail.com>
2024-08-23 22:38:07 +0530

    Added param method

---
+++ learning/method_parameters.java
@@ -0,0 +1,12 @@
+import java.util.*;
+
+public class method_parameters {
+	static void myMethod(String name) {
+		System.out.println(name);
+	}
+
+	public static void main(String[] args) {
+		myMethod("sk");
+		myMethod("hell");
+	}
+}
